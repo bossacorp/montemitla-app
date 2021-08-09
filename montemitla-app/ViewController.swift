6327b17de54e18436b2f3bd08740ba12ff09092a
Ra Acosta <ra.acosta@gmail.com>
2021-08-08 22:02:31 -0500

    trying some new View

--- montemitla-app/ViewController.swift
+++ montemitla-app/ViewController.swift
@@ -11,14 +11,22 @@ import UIKit
 class ViewController: UIViewController {
 
     @IBOutlet weak var monteMitlaImageView: UIImageView!
+    @IBOutlet weak var versionLabel: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
     }
 
     @IBAction func monteMitlaLogoPressed(_ sender: UIButton) {
-        print("Monte Mitla Logo tapped");
-        monteMitlaImageView.alpha = 0.5;
+        print("\(sender.currentTitle!) \(versionLabel.text!)" )
+        
+        if versionLabel.text == "v0.0.1" {
+            print("We should be Legacy...")
+        }
+        //monteMitlaImageView.alpha = 0.5;
+        
+        let mezanineVC = mezanineViewController()
+        self.present(mezanineVC, animated: true, completion: nil)
     }
 }
 
---
+++ montemitla-app/mezanineViewController.swift
@@ -0,0 +1,30 @@
+//
+//  mezanineViewController.swift
+//  montemitla-app
+//
+//  Created by Ra Acosta on 08/08/21.
+//  Copyright © 2021 bossacorp. All rights reserved.
+//
+
+import UIKit
+
+class mezanineViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
